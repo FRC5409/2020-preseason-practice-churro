d3cf4a257e275a50312535b0fda0aa1edfcf3439
Keith Davies <32884707+Waneden@users.noreply.github.com>
2019-11-11 21:55:41 -0500

    Updated Drive command to new subsystem

--- src/main/java/org/usfirst/frc5409/Testrobot/commands/Drive.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/commands/Drive.java
@@ -1,29 +1,31 @@
 package org.usfirst.frc5409.Testrobot.commands;
 
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.command.Command;
-import org.usfirst.frc5409.Testrobot.subsystems.Drivetrain;
 import org.usfirst.frc5409.Testrobot.util.JoystickType;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.XboxController;
 import org.usfirst.frc5409.Testrobot.Robot;
 
 public class Drive extends Command {
 
     public Drive() {
         super("Drive");
+
         requires(Robot.drivetrain);
     }
 
     @Override
     protected void initialize() {
-        Robot.drivetrain.updateDrive(0, 0);
+        Robot.drivetrain.reset();
     }
 
     @Override
     protected void execute() {
         XboxController joystick = Robot.oi.getJoystick(JoystickType.MAIN);
+
         final double axis_vel   = joystick.getRawAxis(3) - joystick.getRawAxis(2);
         final double axis_rot   = joystick.getRawAxis(0);
-        Robot.drivetrain.updateDrive(axis_vel, axis_rot); //Have to flip params for some reason...
+
+        Robot.drivetrain.arcadeDrive(axis_vel, axis_rot);
     }
 
     @Override
@@ -33,11 +35,11 @@ public class Drive extends Command {
 
     @Override
     protected void end() {
-        Robot.drivetrain.updateDrive(0, 0);
+        Robot.drivetrain.reset();
     }
 
     @Override
     protected void interrupted() {
-        Robot.drivetrain.updateDrive(0, 0);
+        Robot.drivetrain.reset();
     }
 }
