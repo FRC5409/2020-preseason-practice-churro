6f340ff31316f8d5ab0dc52480a45e0a48ebf0a7
Keith Davies <32884707+Waneden@users.noreply.github.com>
2019-11-11 21:54:15 -0500

    Polish

--- src/main/java/org/usfirst/frc5409/Testrobot/util/Range.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/util/Range.java
@@ -1,14 +1,12 @@
 package org.usfirst.frc5409.Testrobot.util;
 
-
-
 //Numerical range, very unsafe generic class
-public class Range<T> {
+public class Range<T extends Number> {
+    public final T min;
+    public final T max;
+
     public Range(T min, T max) {
         this.min = min;
         this.max = max;
     }
-
-    public final T min;
-    public final T max;
 }
