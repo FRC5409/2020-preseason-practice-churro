0b9804eaa8b1e462088f1e3f8da3f97a801904f2
Keith Davies <keithdaviesjr81@gmail.com>
2019-11-12 17:10:36 -0500

    Added limelight subsystem for basic limelight control

--- src/main/java/org/usfirst/frc5409/Testrobot/limelight/lltype.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/limelight/lltype.java
@@ -1,21 +1,59 @@
 package org.usfirst.frc5409.Testrobot.limelight;
 
 //Limelight type
-public class lltype {
-    private class type {
-        public void type(byte ) {
-            
+public final class lltype {
+    public enum CameraMode {
+        MODE_VISION (0),
+        MODE_DRIVER (1);
+
+        CameraMode(double value) {
+            this.value = value;
         }
 
-        public byte get() {
-            return 
+        public double get() {
+            return value;
         }
 
-        private final byte value;
+        private final double value;
     }
 
-    public enum CameraMode {
-        MODE_VISION = 0,
-        MODE_DRIVER
+    public enum LedMode {
+        LED_PIPELINE(0),
+        LED_OFF     (1),
+        LED_BLINK   (2),
+        LED_ON      (3);
+
+        LedMode(double value) {
+            this.value = value;
+        }
+
+        public double get() {
+            return value;
+        }
+
+        private final double value;
+    }
+
+    public enum PipelineIndex {
+        PIPELINE_0  (0),
+        PIPELINE_1  (1),
+        PIPELINE_2  (2),
+        PIPELINE_3  (3),
+        PIPELINE_4  (4),
+        PIPELINE_5  (5),
+        PIPELINE_6  (6),
+        PIPELINE_7  (7),
+        PIPELINE_8  (8),
+        PIPELINE_9  (9);
+
+        PipelineIndex(double value) {
+            this.value = value;
+        }
+
+        public double get() {
+            return value;
+        }
+
+        private final double value;
     }
 }
