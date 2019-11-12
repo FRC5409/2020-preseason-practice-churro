67fe2a15041979eb2b79e6d8fe4301783dd4acf0
KkeithD <keithdaviesjr81@gmail.com>
2019-11-12 09:50:24 -0500

    Beginning of limelight implementation

---
+++ src/main/java/org/usfirst/frc5409/Testrobot/limelight/lltype.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc5409.Testrobot.limelight;
+
+//Limelight type
+public class lltype {
+    private class type {
+        public void type(byte ) {
+            
+        }
+
+        public byte get() {
+            return 
+        }
+
+        private final byte value;
+    }
+
+    public enum CameraMode {
+        MODE_VISION = 0,
+        MODE_DRIVER
+    }
+}
