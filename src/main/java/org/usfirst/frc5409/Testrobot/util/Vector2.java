85818d64594cb8b1c0be8e6f4dacafd9839cfb5a
Keith Davies <keithdaviesjr81@gmail.com>
2019-11-14 16:48:04 -0500

    Forgot public constructors

--- src/main/java/org/usfirst/frc5409/Testrobot/util/Vector2.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/util/Vector2.java
@@ -3,12 +3,12 @@ package org.usfirst.frc5409.Testrobot.util;
 public class Vector2 {
     public double x, y;
 
-    Vector2() {
+    public Vector2() {
         x = 0;
         y = 0;
     }
 
-    Vector2(double x, double y) {
+    public Vector2(double x, double y) {
         this.x = x;
         this.y = y;
     }
