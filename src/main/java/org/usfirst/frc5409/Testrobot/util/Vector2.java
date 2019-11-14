e93c4990cd7e64dade246d945fe4293865d7a979
Keith Davies <keithdaviesjr81@gmail.com>
2019-11-14 16:44:42 -0500

    Added simple vector classes in 2 and 3 dimensions

---
+++ src/main/java/org/usfirst/frc5409/Testrobot/util/Vector2.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc5409.Testrobot.util;
+
+public class Vector2 {
+    public double x, y;
+
+    Vector2() {
+        x = 0;
+        y = 0;
+    }
+
+    Vector2(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/main/java/org/usfirst/frc5409/Testrobot/util/Vector3.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc5409.Testrobot.util;
+
+public class Vector3 {
+    public double x, y, z;
+
+    Vector3() {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    Vector3(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
