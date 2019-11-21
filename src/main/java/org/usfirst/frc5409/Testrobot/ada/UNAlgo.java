3ada9dd52d69a9c5fe87ccfea02a476daf5a3c89
Keith Davies <keithdaviesjr81@gmail.com>
2019-11-20 22:41:26 -0500

    Fully implemented with corrected math and cleaned up equations

--- src/main/java/org/usfirst/frc5409/Testrobot/ada/UNAlgo.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/ada/UNAlgo.java
@@ -1,21 +1,21 @@
 package org.usfirst.frc5409.Testrobot.ada;
 //https://www.geogebra.org/geometry/mj6c8auz
 import org.usfirst.frc5409.Testrobot.util.Vector2;
-import org.usfirst.frc5409.Testrobot.util.Vector3;
 
 public class UNAlgo {
-    public double kD;
-    public double kR;
-    public double db;
+    public double  kD;
+    public double  kR;
+    public double  db;
+    public double  mo;
+    private double fl;
 
     private boolean is_first_iter;
-    private double  fl;
 
-
-    public UNAlgo(double kD, double kR, double db) {
+    public UNAlgo(double kD, double kR, double db, double mo) {
         this.kD = kD;
         this.kR = kR;
         this.db = db;
+        this.mo = mo;
 
         is_first_iter = true;
     }
@@ -24,65 +24,127 @@ public class UNAlgo {
         is_first_iter = true;
     }
 
-    public double[] compute(Vector2 targ, Vector2 n1, Vector2 org, Vector2 n2) {
-        Vector2 C = computeGravitator(targ, n1, org, n2);
-        final Vector2 n3 = C.sub(org).norm();
+    public double[] compute(Vector2 T, Vector2 nT, Vector2 O, Vector2 nO) {
+        Vector2 nG = new Vector2();
+        Vector2 Gp; //Gets initialized later      
+        double  dg;
+
         if (is_first_iter) {
             is_first_iter = false;
-            fl = Math.signum(n3.dot(new Vector2(-n1.y, n1.x)));
+            fl = sgn((O.y-T.y)*nT.x - (O.x-T.x)*nT.y);
         }
 
-        final double df = clamp(fl*Math.acos(n2.dot(n3))*kR);
-        final double ddist = clamp(deadband(targ.distance(org)*kD));
+        //Code block for determining Gp, logic is heavily condensed.
+        //Will explain l8r
+        assignBest: {
+            Vector2 Pi = computePi(T, nT, O, nO);
+            assignPo: { 
+                checkPi: {
+                    if (Pi == null) //No intersection
+                        break checkPi;
+                    else {
+                        final double dot = (Pi.x-O.x)*nO.x + (Pi.y-O.y)*nO.y;
+                        if (dot < 0) //Behind Ray
+                            break checkPi;
+                    }
+                    break assignPo;
+                }
+                
+                Gp = computePo(T, O, nT);
+                dg = computeD(T, O, Gp);
+                
+                System.out.printf("Gp: (%f, %f)\n", Gp.x, Gp.y);
+                break assignBest;
+            }
+            
+            Vector2 Po = computePo(T, O, nT);
 
-        return new double[] {df, ddist};
-    }
-    // O - target
-    // B - Origin
-    public Vector2 computeGravitator(Vector2 targ, Vector2 n1, Vector2 org, Vector2 n2) {
-        Vector2 opt = computeOptimize(org, n2);
-        Vector2 insct = computeIntersect(org, n1, targ, n2);
+            double d_i = computeD(T, O, Pi);
+            double d_o = computeD(T, O, Po);
 
-        final double dot = (targ.sub(insct)).dot(n1);
-
-        if (dot < 1)
-            return opt;
-        else {
-            if (perimeter(targ, org, opt) < perimeter(targ, org, insct))
-                return opt;
-            else
-                return insct;
+            //Figure out which optimization is the best and use it;
+            if (d_i < d_o) {
+                Gp = Pi;
+                dg = d_i;
+            } else {
+                Gp = Po;
+                dg = d_o;
+            }
+            
+            System.out.printf("Pi: (%f, %f)\n", Pi.x, Pi.y);
+            System.out.printf("Po: (%f, %f)\n", Po.x, Po.y);
+            System.out.printf("Gp: (%f, %f)\n", Gp.x, Gp.y);
+            break assignBest; //This might not even be necessary
         }
+
+        nG.x = O.x-Gp.x;
+        nG.y = O.y-Gp.y;
+        
+        //normalize
+        final double m = Math.sqrt(nG.x*nG.x + nG.y*nG.y);
+        nG.x /= m;
+        nG.y /= m;
+
+        //Calculate motor output modifiers
+        final double mr = clamp(Math.acos(clamp(nO.x*nG.x + nO.y*nG.y)) * kR * fl);
+        final double md = clamp(deadband(dg/2 * kD));
+
+        //Calculate raw motor outputs
+        final double Rr = md + mr;
+        final double Rl = md - mr;
+
+        double s = 1;
+        if (Rr > mo || Rr < -mo) 
+            s = Math.abs(Rr);
+        else if (Rl > mo || Rl < -mo)
+            s = Math.abs(Rl);
+
+        return new double[] {Rl/s, Rr/s};
     }
 
-    public double perimeter(Vector2 A, Vector2 B, Vector2 C) {
-        return A.distance(C) + B.distance(C);
+    public double computeD(Vector2 A, Vector2 B, Vector2 C) {
+        return Math.sqrt((A.x-C.x)*(A.x-C.x) + (A.y-C.y)*(A.y-C.y))
+                + Math.sqrt((B.x-C.x)*(B.x-C.x) + (B.y-C.y)*(B.y-C.y));
+    }       
+
+    private Vector2 computePo(Vector2 T, Vector2 O, Vector2 nT) {
+        final double px = T.x - O.x;
+        final double py = T.y - O.y;
+
+        final double oi = (px*px + py*py) / (-2*(px*nT.x + py*nT.y));
+        return new Vector2(T.x + nT.x*oi, T.y + nT.y*oi);
     }
 
-    public Vector2 computeOptimize(Vector2 targ, Vector2 n) {
-        final double a = targ.dot(targ);
-        final double b = -2*targ.dot(n);
-    
-        return targ.add(n.mult(a/b));
+    private Vector2 computePi(Vector2 T, Vector2 nT, Vector2 O, Vector2 nO) {
+        final double c1 = nT.y*T.x - nT.x*T.y;
+        final double c2 = nO.y*O.x - nO.x*O.y;
+        final double w  = nT.y*nO.x - nT.x*nO.y;
+
+        if (w == 0)
+            return null;
+
+        return new Vector2((-nT.x*c2 + nO.x*c1)/w, (nO.y*c1 - nT.y*c2)/w);
     }
 
-    public Vector2 computeIntersect(Vector2 targ, Vector2 n1, Vector2 org, Vector2 n2) {
-        Vector3 pi = new Vector3(n1.x,n1.y, org.x-org.y).cross(
-                     new Vector3(n2.x,n2.y, targ.x-targ.y));
-        return new Vector2(pi.x/pi.z, pi.y/pi.z);
+    private double deadband(double x) {
+        if (x > db && x < -db)
+            return x*Math.signum(x);
+        return x;
     }
 
-    private double deadband(double value) {
-        if (value > db && value < -db)
-            return value*Math.signum(value);
-        return value;
+    private double clamp(double x) {
+        if (x > mo)
+            return mo;
+        else if (x < -mo)
+            return -mo;
+        return x;
     }
 
-    private double clamp(double value) {
-        if (value > 1)
+    private double sgn(double x) {
+        if (x > 0)
             return 1;
-        else if (value < -1)
+        else if (x < 0)
             return -1;
-        return value;
+        return 0;
     }
 }
