31e6fe50f247ba6776d68eb3580938884821c78b
Keith Davies <keithdaviesjr81@gmail.com>
2019-11-25 17:51:58 -0500

    Javadoc

--- src/main/java/org/usfirst/frc5409/Testrobot/util/Vector2.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/util/Vector2.java
@@ -1,23 +1,58 @@
 package org.usfirst.frc5409.Testrobot.util;
 
+/**
+ * Simple 2D Cartesian plane
+ * point / vector / coordinate.
+ */
 public class Vector2 {
-    public double x, y;
-
+    /**
+     * Construct Blank Vector2.
+     */
     public Vector2() {
         x = 0;
         y = 0;
     }
 
+    /**
+     * Construct Vector with coordinates.
+     * 
+     * @param x X Coordinate
+     * @param y Y Coordinate
+     */
     public Vector2(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public double dot(Vector2 other) {
-        return (x*other.x + y*other.y);
+    /**
+     * Compute dot product of this vector with
+     * another. (A * B)
+     * 
+     * @param B Another Vector
+     * 
+     * @return Dot product
+     */
+    public double dot(Vector2 B) {
+        return (x*B.x + y*B.y);
     }
 
+    /**
+     * Compute magnitude of this vector.
+     * |A|
+     * 
+     * @return Magnitude of this vector
+     */
     public double magnitude() {
         return Math.sqrt(x*x + y*y);
     }
+
+    /**
+     * X Coordinate
+     */
+    public double x;
+    
+    /**
+     * Y Coordinate
+     */
+    public double y;
 }
--- src/main/java/org/usfirst/frc5409/Testrobot/util/Vector3.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/util/Vector3.java
@@ -1,18 +1,45 @@
 package org.usfirst.frc5409.Testrobot.util;
 
+/**
+ * Simple 3D Cartesian plane
+ * point / vector / coordinate.
+ */
 public class Vector3 {
-    public double x, y, z;
-
+    /**
+     * Construct Blank Vector3.
+     */
     public Vector3() {
         x = 0;
         y = 0;
         z = 0;
     }
 
+    /**
+     * Construct Vector with coordinates.
+     * 
+     * @param x X Coordinate
+     * @param y Y Coordinate
+     * @param z Z Coordinate
+     */
     public Vector3(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
+
+    /**
+     * X Coordinate
+     */
+    public double x;
+    
+    /**
+     * Y Coordinate
+     */
+    public double y;
+    
+    /**
+     * Z Coordinate
+     */
+    public double z;
 }
 
