cf3fb67550c4622cd0221e6ff1bfde401c17394d
Keith Davies <keithdaviesjr81@gmail.com>
2019-11-25 17:41:43 -0500

    added Javadoc

--- src/main/java/org/usfirst/frc5409/Testrobot/navx/Regs.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/navx/Regs.java
@@ -1,10 +1,28 @@
 package org.usfirst.frc5409.Testrobot.navx;
 
+/**
+ * Simple class for specifying a group
+ * of registers on the NavX system.
+ */
 final class Regs {
+    /**
+     * Constructs Regs container.
+     * 
+     * @param rx Starting register
+     * @param ex Ending register
+     */
     public Regs(byte rx, byte ex) {
         this.rx = rx;
         this.nx = (byte) (ex-rx);
     }
-    public final byte rx; //Starting register
-    public final byte nx; //Num of registers
+
+    /**
+     * Starting register
+     */
+    public final byte rx;
+
+    /**
+     * Number of subsequent registers
+     */
+    public final byte nx;
 };
