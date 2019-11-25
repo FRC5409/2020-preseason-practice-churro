f96794cd56e5ec47bce202ce6564312b591612cf
Keith Davies <keithdaviesjr81@gmail.com>
2019-11-25 17:46:36 -0500

    Added Javadoc

--- src/main/java/org/usfirst/frc5409/Testrobot/navx/Conv.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/navx/Conv.java
@@ -6,6 +6,14 @@ package org.usfirst.frc5409.Testrobot.navx;
  * [Raw bytes] -> [Java type]
  */
 final class Conv {
+    /**
+     * Decode Signed Short. (16-bit)
+     * 
+     * @param data Raw byte data
+     * @param i Index offset
+     * 
+     * @return Signed short
+     */
     public static short decodeSignedShort(byte data[], int i) { 
         return (short) ( 
             (data[i    ] << 8) | 
@@ -13,6 +21,14 @@ final class Conv {
         );
     }
 
+    /**
+     * Decode Unsigned Short. (16-bit)
+     * 
+     * @param data Raw byte data
+     * @param i Index offset
+     * 
+     * @return Unsigned short
+     */
     public static int decodeUnsignedShort(byte data[], int i) { //Test this
         return (int) ( 
             (data[i    ] << 8) | 
@@ -20,6 +36,14 @@ final class Conv {
         );
     }
 
+    /**
+     * Decode Signed Integer. (32-bit)
+     * 
+     * @param data Raw byte data
+     * @param i Index offset
+     * 
+     * @return Signed Integer
+     */
     public static int decodeSignedInt(byte data[], int i) {
         return (int) ( 
             (data[i + 0] << 24) |
@@ -29,6 +53,14 @@ final class Conv {
         );
     }
 
+    /**
+     * Decode Unsigned Integer. (32-bit)
+     * 
+     * @param data Raw byte data
+     * @param i Index offset
+     * 
+     * @return Unsigned Integer
+     */
     public static long decodeUnsignedInt(byte data[], int i) { //Test this
         return (int) ( 
             (data[i + 0] << 24) |
@@ -38,26 +70,62 @@ final class Conv {
         );
     }
 
-    public static double decodeSignedHundreths(byte data[], int i) {
+    /**
+     * Decode Signed Hundredths. (-327.68 to 327.67)
+     * 
+     * @param data Raw byte data
+     * @param i Index offset
+     * 
+     * @return Double floating-point Number
+     */
+    public static double decodeSignedHundredths(byte data[], int i) {
         return (double) (decodeSignedShort(data, i)) / 100.0d;
     }
 
-    public static double decodeUnsignedHundreths(byte data[], int i) {
+    /**
+     * Decode Unsigned Hundredths. (0.0 to 655.35)
+     * 
+     * @param data Raw byte data
+     * @param i Index offset
+     * 
+     * @return Double floating-point Number
+     */
+    public static double decodeUnsignedHundredths(byte data[], int i) {
         return (double) (decodeUnsignedShort(data, i)) / 100.0d;
     }
 
+    /**
+     * Decode Signed Thousandths. (-32.768 to 32.767)
+     * 
+     * @param data Raw byte data
+     * @param i Index offset
+     * 
+     * @return Double floating-point Number
+     */
     public static double decodeSignedThousandths(byte data[], int i) {
         return (double) (decodeSignedShort(data, i)) / 1000.0d;
     }
 
-    public static double decodeUnsignedThousandths(byte data[], int i) {
-        return (double) (decodeUnsignedShort(data, i)) / 1000.0d;
-    }
-
+    /**
+     * Decode Signed Pi. (-2 to 2)
+     * 
+     * @param data Raw byte data
+     * @param i Index offset
+     * 
+     * @return Double floating-point Number
+     */
     public static double decodeSignedPiRadians(byte data[], int i) {
         return (double) (decodeSignedShort(data, i)) / 16384.0d;
     }
 
+    /**
+     * Decode Q16.16 Number. (-32768.9999 to 32767.9999)
+     * 
+     * @param data Raw byte data
+     * @param i Index offset
+     * 
+     * @return Double floating-point Number
+     */
     public static double decodeQ1616(byte data[], int i) {
         return (double) (decodeSignedInt(data, i)) / 65536.0d;
     }
