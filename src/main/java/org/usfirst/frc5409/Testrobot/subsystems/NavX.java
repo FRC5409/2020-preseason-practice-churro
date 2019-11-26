db7604f99b2415fffa714772932f85ab93d1b6e0
Keith Davies <keithdaviesjr81@gmail.com>
2019-11-26 15:24:42 -0800

    Began implementation of NavX subsystem

---
+++ src/main/java/org/usfirst/frc5409/Testrobot/subsystems/NavX.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc5409.Testrobot.subsystems;
+
+import org.usfirst.frc5409.Testrobot.navx.SPICom;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class NavX extends Subsystem {
+    private SPICom m_spi_com;
+
+
+    /**
+     * Initializes NavX Subsystem
+     */
+    NavX() {
+        m_spi_com = new SPICom();
+
+        //Initialize communication with NavX
+        boolean res = m_spi_com.init();
+
+    }
+
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+}
