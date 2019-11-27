827a371831e387d59808db67874c2168fb961dd0
Keith Davies <keithdaviesjr81@gmail.com>
2019-11-27 18:12:31 -0500

    Added fused heading to YPRH data packet

--- src/main/java/org/usfirst/frc5409/Testrobot/navx/packet/YPRHData.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/navx/packet/YPRHData.java
@@ -3,33 +3,36 @@ package org.usfirst.frc5409.Testrobot.navx.packet;
 /**
  * Gryoscopic Orientation (Y P R H) data container.
  * Packaged from gyro data on NavX MXP.
- * [Yaw, Pitch, Roll, Heading]
+ * [Yaw, Pitch, Roll, Heading, Fused Heading]
  */
 public class YPRHData {
     /**
-     * Construct YPRH data container.
+     * Construct blank YPRH data container.
      */
     YPRHData() {
-        yaw = 0;
-        ptch = 0;
-        roll = 0;
-        head = 0;
+        yaw   = 0;
+        ptch  = 0;
+        roll  = 0;
+        head  = 0;
+        fhead = 0;
     }
 
     /**
      * Construct YPRH data container with
      * gyro data parameters.
      * 
-     * @param yaw  Gyro Yaw
-     * @param ptch Gyro Pitch
-     * @param roll Gyro Roll
-     * @param head Gyro Heading
+     * @param yaw   Gyro Yaw
+     * @param ptch  Gyro Pitch
+     * @param roll  Gyro Roll
+     * @param head  Gyro Heading
+     * @param fhead Gyro Fused Heading
      */
-    YPRHData(double yaw, double ptch, double roll, double head) {
+    YPRHData(double yaw, double ptch, double roll, double head, double fhead) {
         this.yaw = yaw;
         this.ptch = ptch;
         this.roll = roll;
         this.head = head;
+        this.fhead = fhead;
     }
 
     /**
@@ -48,7 +51,9 @@ public class YPRHData {
     public double roll;
 
     /**
-     * Gyro Heading
+     * Gyro Fused Heading
      */
     public double head;
+
+    public double fhead;
 }
