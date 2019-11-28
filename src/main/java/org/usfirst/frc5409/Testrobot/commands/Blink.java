5ab226d2ecd619c9a4601b71a3246b2edbfdc3d1
KkeithD <keithdaviesjr81@gmail.com>
2019-11-28 09:48:27 -0500

    Updated blink command. It's now triggered from an "A" button press

--- src/main/java/org/usfirst/frc5409/Testrobot/commands/Blink.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/commands/Blink.java
@@ -3,9 +3,10 @@ package org.usfirst.frc5409.Testrobot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc5409.Testrobot.Robot;
 import org.usfirst.frc5409.Testrobot.limelight.*;
+import org.usfirst.frc5409.Testrobot.util.JoystickType;
 
 public class Blink extends Command {
-    public static long c_blink_period_ms = 1000;
+    private boolean m_led_on = false;
 
     public Blink() {
         super("Blink");
@@ -14,14 +15,19 @@ public class Blink extends Command {
 
     @Override
     protected void initialize() {
+        Robot.limelight.setLedMode(LedMode.LED_OFF);
     }
 
     @Override
     protected void execute() {
-        Robot.limelight.setLedMode(LedMode.LED_ON);
-        	sleep();
-        Robot.limelight.setLedMode(LedMode.LED_OFF);
-        	sleep();
+        boolean pressed = Robot.oi.getJoystick(JoystickType.MAIN).getAButtonPressed();
+
+        m_led_on = !m_led_on;
+
+        if (m_led_on)
+            Robot.limelight.setLedMode(LedMode.LED_ON);
+        else
+            Robot.limelight.setLedMode(LedMode.LED_OFF);
     }
 
     @Override
@@ -31,19 +37,11 @@ public class Blink extends Command {
 
     @Override
     protected void end() {
- 
+        Robot.limelight.setLedMode(LedMode.LED_OFF);
     }
 
     @Override
     protected void interrupted() {
 
-    }
-
-    private void sleep() {
-        try {
-            Thread.sleep(c_blink_period_ms);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
