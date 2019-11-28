56c25cf4531a630eb98b7c13052296956a242f0f
Keith Davies <keithdaviesjr81@gmail.com>
2019-11-27 22:45:04 -0500

    Added some docs

--- src/main/java/org/usfirst/frc5409/Testrobot/subsystems/NavX.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/subsystems/NavX.java
@@ -4,19 +4,28 @@ import org.usfirst.frc5409.Testrobot.navx.SPICom;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-
+/**
+ * NavX subsystem.
+ * 
+ * Facilitates the control and accessing
+ * of Kaui Labs hardware through a 
+ * software interface.
+ */
 public class NavX extends Subsystem {
     private SPICom m_spi_com;
 
-
     /**
-     * Initializes NavX Subsystem
+     * Initialize NavX Subsystem
      */
     public NavX() {
         m_spi_com = new SPICom();
-        //Initialize communication with NavX
-        boolean res = m_spi_com.init();
-
+        
+        //Establish communication with NavX
+        if (m_spi_com.init()) {
+            //success
+        } else {
+            //Failure
+        }
     }
 
 
