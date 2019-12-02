c32863ad372b35b0b53ca10da5777ff77c48fd94
Keith Davies <keithdaviesjr81@gmail.com>
2019-12-01 21:23:46 -0500

    Added vision tracking command

--- src/main/java/org/usfirst/frc5409/Testrobot/commands/VisionTrak.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/commands/VisionTrak.java
@@ -1,19 +1,22 @@
 package org.usfirst.frc5409.Testrobot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import org.usfirst.frc5409.Testrobot.Robot;
+import org.usfirst.frc5409.Testrobot.ada.UNAlgo;
 import org.usfirst.frc5409.Testrobot.limelight.LedMode;
+import org.usfirst.frc5409.Testrobot.limelight.TrackMatrix;
 import org.usfirst.frc5409.Testrobot.util.JoystickType;
+import org.usfirst.frc5409.Testrobot.util.Vector2;
 
 public class VisionTrak extends Command {
-    private boolean m_debounce;
-
+    UNAlgo algo;
 
     public VisionTrak() {
         super("VisionTrak");
 
-        m_debounce = false;
-
+        algo = new UNAlgo(0, 0, 0, 1, 0);
         requires(Robot.drivetrain);
         requires(Robot.limelight);
         requires(Robot.navX);
@@ -21,6 +24,10 @@ public class VisionTrak extends Command {
 
     @Override
     protected void initialize() {
+        SmartDashboard.putNumber("kD", 0);
+        SmartDashboard.putNumber("kR", 0);
+        SmartDashboard.putNumber("wd", 0);
+
         Robot.limelight.setLedMode(LedMode.LED_ON);
         Robot.drivetrain.reset();
         Robot.navX.zeroYaw();
@@ -28,7 +35,23 @@ public class VisionTrak extends Command {
 
     @Override
     protected void execute() {
-        
+        algo.kD = SmartDashboard.getNumber("kD", 0);
+        algo.kD = SmartDashboard.getNumber("kR", 0);
+        algo.wd = SmartDashboard.getNumber("wd", 0);
+
+        if (Robot.limelight.hasTarget()) {
+            TrackMatrix tm = Robot.limelight.getCameraTrack();
+            double y_n = tm.yaw/180 * Math.PI; //Asuuming the rotation is not in radians
+
+            double mo[] = algo.compute(
+                new Vector2(0,0),
+                new Vector2(0,-1),
+                new Vector2(tm.x, tm.z),
+                new Vector2(Math.cos(y_n), Math.sin(y_n)));
+
+            Robot.drivetrain.tankDrive(mo[0], mo[1]);
+        } else
+            Robot.drivetrain.reset();
     }
 
     @Override
