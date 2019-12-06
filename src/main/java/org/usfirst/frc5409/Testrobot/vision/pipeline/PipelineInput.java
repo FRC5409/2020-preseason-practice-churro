f26a258e1115eaca514108c9aab6d6f6a0b5acf6
KkeithD <keithdaviesjr81@gmail.com>
2019-12-06 11:19:04 -0500

    Beginning of vision implementation

---
+++ src/main/java/org/usfirst/frc5409/Testrobot/vision/pipeline/PipelineInput.java
@@ -0,0 +1,6 @@
+package org.usfirst.frc5409.Testrobot.vision.pipeline;
+
+public abstract class PipelineInput {
+
+    public abstract <T> T 
+}
---
+++ src/main/java/org/usfirst/frc5409/Testrobot/vision/pipeline/VisionPipeline.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc5409.Testrobot.vision.pipeline;
+
+public abstract class VisionPipeline {
+    public VisionPipeline() {
+
+    }
+}
