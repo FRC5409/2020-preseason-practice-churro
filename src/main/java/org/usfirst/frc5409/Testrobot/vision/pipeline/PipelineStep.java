4c2b683d72aba2aafffdecea30a4d39be8e584e7
Keith Davies <keithdaviesjr81@gmail.com>
2019-12-08 22:43:24 -0500

    Updated class naming

--- src/main/java/org/usfirst/frc5409/Testrobot/vision/pipeline/PipelineStep.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/vision/pipeline/PipelineStep.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc5409.Testrobot.vision.pipeline;
 
-public interface PipelineStep<T extends PipelineInput, O extends PipelineOutput> {
-    public O process(T input);
+@FunctionalInterface
+public interface PipelineStep <T extends PipelineData, V extends PipelineData> {
+    public V process(T input);
 }
