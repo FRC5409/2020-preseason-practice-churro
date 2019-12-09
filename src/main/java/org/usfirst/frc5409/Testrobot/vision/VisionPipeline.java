9614b592399178be1618c94635a3b589fd460793
Keith Davies <keithdaviesjr81@gmail.com>
2019-12-08 23:45:26 -0500

    Basic VisionPipeline implementation

--- src/main/java/org/usfirst/frc5409/Testrobot/vision/VisionPipeline.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/vision/VisionPipeline.java
@@ -1,7 +1,14 @@
 package org.usfirst.frc5409.Testrobot.vision;
 
-public abstract class VisionPipeline {
+import org.usfirst.frc5409.Testrobot.vision.pipeline.PipelineData;
+import org.usfirst.frc5409.Testrobot.vision.structure.PipelineBranch;
+
+public abstract class VisionPipeline<T extends PipelineData, R extends PipelineData> {
+    public PipelineBranch main_branch;
+
     public VisionPipeline() {
-        
+        main_branch = new PipelineBranch();
     }
+
+    public abstract T process(R input);
 }
