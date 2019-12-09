3b8b89f80ffa1d02b6606c0184b3eca778cdd955
Keith Davies <keithdaviesjr81@gmail.com>
2019-12-09 18:37:40 -0500

    protected main branch

--- src/main/java/org/usfirst/frc5409/Testrobot/vision/VisionPipeline.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/vision/VisionPipeline.java
@@ -4,7 +4,7 @@ import org.usfirst.frc5409.Testrobot.vision.pipeline.PipelineData;
 import org.usfirst.frc5409.Testrobot.vision.structure.PipelineBranch;
 
 public abstract class VisionPipeline<T extends PipelineData, R extends PipelineData> {
-    public PipelineBranch main_branch;
+    protected PipelineBranch main_branch;
 
     public VisionPipeline() {
         main_branch = new PipelineBranch();
