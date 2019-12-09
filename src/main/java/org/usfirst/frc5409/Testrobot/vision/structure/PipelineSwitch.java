a4e61625112c50340d47e0e8e52efcd52c21a356
Keith Davies <keithdaviesjr81@gmail.com>
2019-12-08 22:43:14 -0500

    Updated class naming

--- src/main/java/org/usfirst/frc5409/Testrobot/vision/structure/PipelineSwitch.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/vision/structure/PipelineSwitch.java
@@ -1,15 +1,15 @@
 package org.usfirst.frc5409.Testrobot.vision.structure;
 
-import org.usfirst.frc5409.Testrobot.vision.control.SwitchController;
 import org.usfirst.frc5409.Testrobot.vision.exception.*;
 import org.usfirst.frc5409.Testrobot.vision.pipeline.*;
+import org.usfirst.frc5409.Testrobot.vision.control.*;
 
 public class PipelineSwitch extends PipelineStructure implements PipelineStep {
-    protected SwitchController m_switch;
-    protected PipelineStep     m_step_off = null;
-    protected PipelineStep     m_step_on = null;
+    protected PipelineStep  m_step_off = null;
+    protected PipelineStep  m_step_on = null;
+    protected PipeSwitch    m_switch;
 
-    public PipelineSwitch(SwitchController controller) {
+    public PipelineSwitch(PipeSwitch controller) {
         m_switch = controller;
     }
 
@@ -35,7 +35,7 @@ public class PipelineSwitch extends PipelineStructure implements PipelineStep {
     }
 
     @Override
-    public PipelineOutput process(PipelineInput input) {
+    public PipelineData process(PipelineData input) {
         if (m_switch.get())
             return m_step_on.process(input);
         else
