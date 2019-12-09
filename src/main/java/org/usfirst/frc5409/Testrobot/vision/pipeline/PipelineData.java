0f7b111c09c2caa5ce863dd8acd4bd8d0555baed
Keith Davies <keithdaviesjr81@gmail.com>
2019-12-08 22:42:44 -0500

    Unified PipelineInput and PipelineOutput under one common PipelineData class

--- src/main/java/org/usfirst/frc5409/Testrobot/vision/pipeline/PipelineData.java
+++ src/main/java/org/usfirst/frc5409/Testrobot/vision/pipeline/PipelineData.java
@@ -3,14 +3,21 @@ package org.usfirst.frc5409.Testrobot.vision.pipeline;
 import org.usfirst.frc5409.Testrobot.vision.control.ChoiceData;
 import org.usfirst.frc5409.Testrobot.vision.exception.DataConversionException;
 
-public abstract class PipelineData {
-
-    public PipelineInput asInput() {
-        return (PipelineInput) (this);
+public abstract class PipelineData implements Cloneable {
+    public PipelineData clone() { //TODO: This is so incredibly bad please this asap, absolutely disgusting and unsafe.
+        try {
+            return (PipelineData) super.clone();
+        } catch (Exception e) {
+            return null;
+        }
+    }  
+    
+    public <T extends PipelineData> T unpack() {
+		return (T) (this);
     }
 
-    public PipelineOutput asOutput() {
-        return (PipelineOutput) (this);
+    public PipelineData pack() {
+        return (PipelineData) (this);
     }
 
     public ChoiceData asChoice() {
