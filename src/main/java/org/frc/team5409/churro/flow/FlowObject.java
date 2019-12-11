50a959542a39c14aaf29360940bad4d68958d0b7
Keith Davies <keithdaviesjr81@gmail.com>
2019-12-11 17:43:16 -0500

    Added type assertion for flow tasks

--- src/main/java/org/frc/team5409/churro/flow/FlowObject.java
+++ src/main/java/org/frc/team5409/churro/flow/FlowObject.java
@@ -1,9 +1,15 @@
 package org.frc.team5409.churro.flow;
 
 public abstract class FlowObject<T> {
+    private final FlowType<T> m_type  = new FlowType<T>();
+
     T object;
 
     public FlowObject(T copy) {
         object = copy;
+    }
+    
+    public final FlowType<T> getType() {
+        return m_type;
     }
 }
--- src/main/java/org/frc/team5409/churro/flow/FlowTask.java
+++ src/main/java/org/frc/team5409/churro/flow/FlowTask.java
@@ -1,16 +1,27 @@
 package org.frc.team5409.churro.flow;
 
-class FlowTask<T> implements Runnable {
-    private FlowObject<T>  m_drop;
-    private FlowNode       m_node;
+import org.frc.team5409.churro.flow.exception.FlowTypeException;
 
-    public FlowTask(FlowNode node, FlowObject<T> drop) {
+class FlowTask implements Runnable {
+    private FlowObject m_drop;
+    private FlowNode   m_node;
+
+    public FlowTask(FlowNode node, FlowObject drop) {
         m_node = node;
         m_drop = drop;
     }
 
     @Override
     public void run() {
+        assertTypes(m_node.getDripType(), m_drop.getType());
+        
         m_node.flow(m_drop);
+    }
+
+    private static final <T,R> void assertTypes(FlowType<T> type_a, FlowType<R> type_b) {
+        if (!type_a.clazz.isAssignableFrom(type_b.clazz))
+            throw new FlowTypeException(String.format("Attempted to flow %s type through non-convertible %s type. Check Code",
+                type_a.clazz.getSimpleName(), 
+                    type_b.clazz.getSimpleName()));
     }
 }
