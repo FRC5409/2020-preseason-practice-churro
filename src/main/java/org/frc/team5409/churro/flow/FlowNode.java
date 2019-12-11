3cf4948a4551b8cf899231b10428fab86b916d99
Keith Davies <keithdaviesjr81@gmail.com>
2019-12-11 17:44:15 -0500

    Added functions for connecting flows with nodes

--- src/main/java/org/frc/team5409/churro/flow/FlowNode.java
+++ src/main/java/org/frc/team5409/churro/flow/FlowNode.java
@@ -1,14 +1,77 @@
 package org.frc.team5409.churro.flow;
 
-public abstract class FlowNode {
-    protected FlowNode    m_children[];
-    protected FlowNode    m_parent;
+import java.util.Arrays;
 
-    private   FlowNetwork m_network;
+import org.frc.team5409.churro.flow.exception.FlowException;
 
-    protected <T> void onDrip(FlowObject<T> drip) {};
+public abstract class FlowNode<T, R> {
+    private final FlowType<T> m_flow_type = new FlowType<T>();
+    private final FlowType<R> m_drip_type = new FlowType<R>();
 
-    protected <T> void flow(FlowObject<T> drop) {
-        m_network.flow(m_children, drop);
+    private FlowNetwork m_network = null;
+
+    private FlowNode    m_children[];
+    private FlowNode    m_parent = null;
+
+    private int         m_this_index = NO_INDEX;
+    private int         m_num_children = 0;
+
+    protected abstract void onFlow(FlowObject<T> drop);
+
+    protected final synchronized int addChild(FlowNode child) {
+        
+        if (child.m_parent != null)
+            throw new FlowException("Attempted to adopt an already parented node.");
+        else if (child.m_network != null)
+            throw new FlowException("Attempted to adopt a child of a different network.");
+        else if
+
+        m_num_children++;
+
+        if (m_num_children > m_children.length)
+            m_children = Arrays.copyOf(m_children, m_num_children);
+
+        m_children[m_num_children-1] = child;
+        child.m_network = m_network;
+        
+        return m_num_children-1;
     }
+
+    protected final synchronized void addParent(FlowNode parent, int child_index) {
+        if (m_parent != null)
+            throw new FlowException("Attempted child a parent node when child already has a parent.");
+        
+        m_parent = parent;
+        m_this_index = child_index;
+    }
+
+    protected final synchronized void joinNetwork(FlowNetwork network) {
+        if (m_network != null)
+            throw new FlowException("Attempted join a network when node was already apart of one.");
+        m_network = network;
+    }
+
+    public final synchronized void flow(FlowObject<T> drop) {
+        onFlow(drop);
+
+        if (m_children.length != 0)
+            m_network.flow(m_children, drop);
+    }
+
+    public final synchronized void flow(FlowNode node, FlowObject<T> drop) {
+        onFlow(drop);
+
+        if (m_children.length != 0)
+            m_network.flow(node, drop);
+    }
+
+    public final FlowType<T> getFlowType() {
+        return m_flow_type;
+    }
+
+    public final FlowType<R> getDripType() {
+        return m_drip_type;
+    }
+
+    private static final int NO_INDEX = -1;
 }
