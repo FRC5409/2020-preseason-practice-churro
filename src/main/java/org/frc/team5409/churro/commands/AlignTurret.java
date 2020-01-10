513979170042499da718328f631f842151b22c38
Keith Davies <keithdaviesjr81@gmail.com>
2020-01-09 21:09:29 -0500

    End of day push

--- src/main/java/org/frc/team5409/churro/commands/AlignTurret.java
+++ src/main/java/org/frc/team5409/churro/commands/AlignTurret.java
@@ -3,6 +3,7 @@ package org.frc.team5409.churro.commands;
 import org.frc.team5409.churro.Robot;
 import org.frc.team5409.churro.control.ServiceManager;
 import org.frc.team5409.churro.services.VisionService;
+import org.frc.team5409.churro.subsystems.TurretControl;
 import org.frc.team5409.churro.util.Vec3;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -10,19 +11,28 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public final class AlignTurret extends Command {
     private VisionService m_service;
-    
-    public AlignTurret() {
+    private TurretControl m_subsystem;
 
+    public AlignTurret() {
         requires(Robot.turretControl);
+        m_subsystem = Robot.turretControl;
     }
 
     @Override
     protected void initialize() {
-        m_service = ServiceManager.get(VisionService.class);
+        m_service = ServiceManager.get("VisionService");
 
         SmartDashboard.setDefaultNumber("Target height", 0);
         SmartDashboard.setDefaultNumber("Robot height", 0);
         SmartDashboard.setDefaultNumber("Turret P", 0);
+        SmartDashboard.setDefaultNumber("Turret I", 0);
+        SmartDashboard.setDefaultNumber("Turret D", 0);
+        SmartDashboard.setDefaultNumber("Camera elevation", 0);
+        SmartDashboard.setDefaultNumber("Zero angle", 90);
+        SmartDashboard.setDefaultNumber("Target rotation", 0);
+        SmartDashboard.setDefaultNumber("Max Rotation Speed", 0);
+
+        m_subsystem.zeroRotation();
     }
 
     @Override
@@ -30,11 +40,13 @@ public final class AlignTurret extends Command {
         if (m_service.hasTarget()) {
             Vec3 tpos = m_service.getTarget();
             
-            final double tx = tpos.x/180 * Math.PI;
+            final double a0 = (SmartDashboard.getNumber("Camera elevation", 0)/180) * Math.PI;
+
+            final double tx = (tpos.x)/180 * Math.PI;
             final double ty = tpos.y/180 * Math.PI;
 
             final double hd = Math.abs(SmartDashboard.getNumber("Target height", 0) - SmartDashboard.getNumber("Robot height", 0));
-            final double dt = hd / (Math.sin(ty+ (10/180*Math.PI))*12);
+            final double dt = hd / (Math.sin(ty+a0)*12);
 
             Vec3 rpos = new Vec3(
                 dt * (Math.cos(ty) *   Math.cos(tx)),
@@ -42,10 +54,17 @@ public final class AlignTurret extends Command {
                 dt *  Math.sin(ty)
             );
 
-            SmartDashboard.putString("Target 2D", String.format("{%f, %f, &f}", tpos.x, tpos.y));
-            SmartDashboard.putString(" Robot 3D", String.format("{%f, %f, &f}", rpos.x, rpos.y, rpos.z));
+            SmartDashboard.putString("Target 2D", String.format("{%f, %f}", tpos.x, tpos.y));
+            SmartDashboard.putString(" Robot 3D", String.format("{%f, %f, %f}", rpos.x, rpos.y, rpos.z));
+            m_subsystem.setRotation(m_subsystem.getRotation()+tpos.x);
         } //else
             //Robot.turretControl.m_pwm4_turret_rotation.set(0);
+
+        m_subsystem.setP(SmartDashboard.getNumber("Turret P", 0));
+        m_subsystem.setI(SmartDashboard.getNumber("Turret I", 0));
+        m_subsystem.setD(SmartDashboard.getNumber("Turret D", 0));
+        //m_subsystem.setRotation(SmartDashboard.getNumber("Target rotation", 0));
+        SmartDashboard.putNumber("Rotation", m_subsystem.getRotation());
     }
 
     @Override
@@ -61,10 +80,10 @@ public final class AlignTurret extends Command {
     }
 
     private void setRotation(double target) {
-        final double speed = (target-Robot.turretControl.getRotation()) * SmartDashboard.getNumber("Turret P", 0);
+        final double speed = (target-m_subsystem.getRotation()) * SmartDashboard.getNumber("Turret P", 0);
 
         SmartDashboard.putNumber("Turret Speed", speed);
 
-        //Robot.turretControl.m_pwm4_turret_rotation.set(speed);
+        //m_subsystem.m_pwm4_turret_rotation.set(speed);
     }
 }
--- src/main/java/org/frc/team5409/churro/subsystems/TurretControl.java
+++ src/main/java/org/frc/team5409/churro/subsystems/TurretControl.java
@@ -1,58 +1,111 @@
 package org.frc.team5409.churro.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.frc.team5409.churro.commands.AlignTurret;
 
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Victor;
 
 public final class TurretControl extends Subsystem {
-    public Victor  m_pwm4_turret_rotation;
-    public Encoder e_qd89_turret_rotation;
+    private Victor  mo_pwm4_turret_rotation;
+    private Encoder en_qd89_turret_rotation;
 
-    public Spark   m_pwm6_turret_flywheel;
-    public Spark   m_pwm7_turret_flywheel;
-    public Encoder e_qd12_turret_flywheel;
+    private Spark   mo_pwm6_turret_flywheel;
+    private Spark   mo_pwm7_turret_flywheel;
+    private Encoder en_qd45_turret_flywheel;
+
+    private PIDController m_rot_controller;
 
     public TurretControl() {
-        m_pwm4_turret_rotation = new Victor(4);
+        mo_pwm4_turret_rotation = new Victor(4);
         
-        addChild(m_pwm4_turret_rotation);
+        addChild(mo_pwm4_turret_rotation);
 
-        e_qd89_turret_rotation = new Encoder(8, 9);
-            e_qd89_turret_rotation.setDistancePerPulse(1/4);
+        en_qd89_turret_rotation = new Encoder(8, 9);
+            en_qd89_turret_rotation.setDistancePerPulse(1.0d/2.5d);
+            en_qd89_turret_rotation.setReverseDirection(true);
         
-        addChild(e_qd89_turret_rotation);
+        addChild(en_qd89_turret_rotation);
 
-        m_pwm6_turret_flywheel = new Spark(6);
+        mo_pwm6_turret_flywheel = new Spark(6);
         
-        addChild(m_pwm6_turret_flywheel);
+        addChild(mo_pwm6_turret_flywheel);
 
-        m_pwm7_turret_flywheel = new Spark(7);
+        mo_pwm7_turret_flywheel = new Spark(7);
         
-        addChild(m_pwm7_turret_flywheel);
+        addChild(mo_pwm7_turret_flywheel);
 
-        e_qd12_turret_flywheel = new Encoder(1, 2);
-            e_qd12_turret_flywheel.setDistancePerPulse(1/4);
+        en_qd45_turret_flywheel = new Encoder(4, 5);
+            en_qd45_turret_flywheel.setDistancePerPulse(1/4);
         
-        addChild(e_qd12_turret_flywheel);
+        addChild(en_qd45_turret_flywheel);
+
+        m_rot_controller = new PIDController(0, 0, 0);
     }
 
     public void zeroRotation() {
-        e_qd89_turret_rotation.reset();
+        en_qd89_turret_rotation.reset();
+        synchronized(m_rot_controller) {
+            m_rot_controller.reset();
+        }
+    }
+
+    public synchronized void setRotation(double target) {
+        synchronized(m_rot_controller) {
+            m_rot_controller.setSetpoint(clamp(0, target, 90));
+        }
     }
 
     public double getRotation() {
-        return e_qd89_turret_rotation.getDistance();
+        return en_qd89_turret_rotation.getDistance();
     }
 
+    public void setP(double P) {
+        synchronized(m_rot_controller) {
+            m_rot_controller.setP(P);
+        }
+    }
+
+    public void setI(double I) {
+        synchronized(m_rot_controller) {
+            m_rot_controller.setI(I);
+        }
+    }
+
+    public void setD(double D) {
+        synchronized(m_rot_controller) {
+            m_rot_controller.setD(D);
+        }
+    }
+
+    @Override
+    public void periodic() {
+        double speed;
+        synchronized(m_rot_controller) {
+            speed = m_rot_controller.calculate(getRotation() );
+        }
+
+        SmartDashboard.putNumber("Turret Rotation Speed", speed);
+        double max = SmartDashboard.getNumber("Max Rotation Speed", 0);
+        mo_pwm4_turret_rotation.set(clamp(-max, speed, max));
+    }
 
     @Override
     protected void initDefaultCommand() {
         // TODO Auto-generated method stub
-        setDefaultCommand(new AlignTurret());
+        setDefaultCommand(new AlignTurret()); 
+    }
+    
+    private double clamp(double mn, double v, double mx) {
+        if (v > mx)
+            return mx;
+        else if (v < mn)
+            return mn;
+        return v;
     }
 
 }
