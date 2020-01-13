8e0d68f8cb0453d8ff8f7bde93f9c66c9aef5916
Keith Davies <keithdaviesjr81@gmail.com>
2020-01-13 00:39:20 -0500

    Updated container with new commands

--- src/main/java/org/frc/team5409/churro/RobotContainer.java
+++ src/main/java/org/frc/team5409/churro/RobotContainer.java
@@ -9,11 +9,19 @@ package org.frc.team5409.churro;
 
 import org.frc.team5409.churro.system.SystemContainer;
 
+import org.frc.team5409.churro.control.ServiceManager;
+import org.frc.team5409.churro.services.UserInputService;
+import org.frc.team5409.churro.uinput.IController;
+import org.frc.team5409.churro.uinput.IController.*;
+
 import org.frc.team5409.churro.subsystems.FeederControl;
 import org.frc.team5409.churro.subsystems.TurretControl;
 import org.frc.team5409.churro.subsystems.Drivetrain;
 
 import org.frc.team5409.churro.commands.AlignTurret;
+import org.frc.team5409.churro.commands.Drive;
+import org.frc.team5409.churro.commands.RunFeeder;
+
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -22,10 +30,15 @@ import org.frc.team5409.churro.commands.AlignTurret;
  */
 public class RobotContainer extends SystemContainer {
     protected void initialize() {
+        IController controller = ServiceManager.getService("UserInputService", UserInputService.class).getController(Controller.kMainDriver);
+
         addSubsystem("TurretControl", new TurretControl());
         addSubsystem("FeederControl", new FeederControl());
-        addSubsystem("Drivetrain", new Drivetrain());
+        addSubsystem(   "Drivetrain", new Drivetrain()   );
 
-        addCommand("AlignTurret", new AlignTurret());
+        addCommand(    "AlignTurret", new AlignTurret());
+        addCommand(          "Drive", new Drive()      );
+        addCommand(      "RunFeeder", new RunFeeder()  );
+            controller.getButton(Button.kA).whilePressedLatch(getCommand("RunFeeder"));
     }
 }
